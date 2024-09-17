6de5cd3f644018f7ae162e5580a7d14c68edc863
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-09-17 10:53:55 +0200

    variable nu gemaakt van de leeftijden.

--- ifkeuze1.php
+++ ifkeuze1.php
@@ -3,14 +3,16 @@
 $naam = readline("Geef jouw naam : ");
 $leeftijd = readline("Geef jouw leeftijd : ");
 
+$actueleleeftijd = "21";
+
 echo "Hallo $naam \n";
 
-if ( $leeftijd < 21 ) {
-        echo "Jij bent jonger dan 21 jaar.\n";
+if ( $leeftijd < $actueleleeftijd ) {
+        echo "Jij bent jonger dan $actueleleeftijd jaar.\n";
 }
-if ( $leeftijd > 21 ) {
-        echo "Jij bent ouder dan 21 jaar.\n";
+if ( $leeftijd > $actueleleeftijd ) {
+        echo "Jij bent ouder dan $actueleleeftijd jaar.\n";
 }
-if ( $leeftijd == 21 ) {
-        echo "Jij bent precies 21 jaar.\n";
+if ( $leeftijd == $actueleleeftijd ) {
+        echo "Jij bent precies $actueleleeftijd jaar.\n";
 }
