149ea14ff440a3e49dadecad20999406f8ae39b3
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-09-18 13:52:42 +0200

    Code verder bijgewerkt

--- quiz.php
+++ quiz.php
@@ -1,14 +1,24 @@
 <?php
-$random = rand(1,100);
-$input = readline("Raad het getal: ");
+$random = rand(1, 100);
+$getal = 0.5;
 
-if ($input<1) {
-    exit("voer een getal tussen 1 en 100.");
-}elseif($input>100) {
-    exit("voer een getal tussen 1 en 100.");
+echo "$random\n";
+
+while ($input = readline("Raad het getal tussen 1 en 100: ")) {
+    
+    if ($input < 1 || $input > 100) {
+        echo "Voer een getal tussen 1 en 100 in.\n";
+        exit; 
+    };
+
+
+    if($input < $random) {
+        echo "Je getal ligt er onder\n";
+    }elseif($input > $random) {
+        echo "je getal ligt er boven\n";
+    } else {
+        echo "je hebt de getal goed geraden!\n ";
+        break;
+    }
 }
-
-
-
-
 ?>
