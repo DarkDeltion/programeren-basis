0be7cf27cb5e7140e98be15627ea0ef70cc7b7f8
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-09-18 11:44:08 +0200

    quiz.php toegevoegd

---
+++ quiz.php
@@ -0,0 +1,14 @@
+<?php
+$random = rand(1,100);
+$input = readline("Raad het getal: ");
+
+if ($input<1) {
+    exit("voer een getal tussen 1 en 100.");
+}elseif($input>100) {
+    exit("voer een getal tussen 1 en 100.");
+}
+
+
+
+
+?>
