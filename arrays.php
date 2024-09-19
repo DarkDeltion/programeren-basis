e05b6bb51912653fc05f0b9e5d6aff66e97b3f62
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-09-19 15:41:32 +0200

    arrays.php toegevoegd.

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<?php
+$weekdagen = array('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag');
+
+echo "$weekdagen[0]\n";
+
+echo "$weekdagen[4]\n";
+
+echo "$weekdagen[6]\n";
+
+?>
