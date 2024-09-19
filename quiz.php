da66c6b9981eda941c061909de24d13b6db77bc6
damien <161700122+DarkDeltion@users.noreply.github.com>
2024-09-19 09:27:19 +0200

    Verder bijgewerkt, werkt nu compleet.

--- quiz.php
+++ quiz.php
@@ -1,24 +1,29 @@
 <?php
 $random = rand(1, 100);
-$getal = 0.5;
+$pogingen = 0;
 
 echo "$random\n";
 
-while ($input = readline("Raad het getal tussen 1 en 100: ")) {
+while (($input = readline("Raad het getal tussen 1 en 100: ")) !== false) {
     
-    if ($input < 1 || $input > 100) {
-        echo "Voer een getal tussen 1 en 100 in.\n";
-        exit; 
-    };
-
-
-    if($input < $random) {
-        echo "Je getal ligt er onder\n";
-    }elseif($input > $random) {
-        echo "je getal ligt er boven\n";
-    } else {
-        echo "je hebt de getal goed geraden!\n ";
+    if($input < 1) {
+        echo "kies een getal tussen 1 en 100\n";
         break;
+    }elseif($input > 100) {
+        echo "kies een getal tussen 1 en 100\n";
+        break;
+    }else{
+
+        $pogingen++;
+
+        if($input < $random) {
+            echo "Je getal ligt er onder\n";
+        }elseif($input > $random) {
+            echo "je getal ligt er boven\n";
+        } else {
+            echo "je hebt de getal goed geraden in $pogingen pogingen!\n ";
+            break;
+        }
     }
 }
 ?>
